b868bdfe48ce129cc9fe2a0c56e21726c8f015f1
Sebastian Bauer <BaschtianXD@users.noreply.github.com>
2023-01-03 00:07:46 +0000

    Added file dragdrop and added ffmpeg to devcontainer

--- src/pages/config/[guildid]/sounds/index.tsx
+++ src/pages/config/[guildid]/sounds/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import { useRouter } from "next/router";
 import { trpc } from "../../../../utils/trpc";
 import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline"
-import { useState } from "react";
+import { Reducer, useReducer, useState } from "react";
 import Head from "next/head";
 
 enum DragError {
@@ -12,19 +12,85 @@ enum DragError {
     Filesize
 }
 
-type NewSound = {
+interface NewSound {
     name: string
-    hidden: boolean
+    hidden: boolean,
     file: File | null
 }
 
-function createNewSound(file: File): NewSound {
-    return {
-        name: file.name.slice(0, file.name.lastIndexOf(".")),
-        hidden: false,
-        file
+enum newSoundActionType {
+    Init,
+    InitFromFile,
+    Clear,
+    SetName,
+    ToggleHidden,
+    SetFile
+}
+
+type NewSoundActionInit = {
+    type: newSoundActionType.Init
+}
+type NewSoundActionInitFromFile = {
+    type: newSoundActionType.InitFromFile
+    payload: {
+        file: File
     }
 }
+type NewSoundActionClear = {
+    type: newSoundActionType.Clear
+}
+type NewSoundActionSetName = {
+    type: newSoundActionType.SetName
+    payload: {
+        newName: string
+    }
+}
+type NewSoundActionToggleHidden = {
+    type: newSoundActionType.ToggleHidden
+}
+type NewSoundActionSetFile = {
+    type: newSoundActionType.SetFile
+    payload: {
+        file: File | null
+    }
+}
+
+type NewSoundReducerAction = NewSoundActionInit | NewSoundActionInitFromFile | NewSoundActionClear | NewSoundActionSetName | NewSoundActionToggleHidden | NewSoundActionSetFile
+
+const newSoundReducer: Reducer<NewSound | null, NewSoundReducerAction> = (prevState, action) => {
+    switch (action.type) {
+        case newSoundActionType.Init:
+            return {
+                name: "",
+                hidden: false,
+                file: null
+            }
+        case newSoundActionType.InitFromFile:
+            return {
+                name: action.payload.file.name.slice(0, action.payload.file.name.lastIndexOf(".")),
+                hidden: false,
+                file: action.payload.file
+            }
+        case newSoundActionType.Clear:
+            return null
+        case newSoundActionType.SetName:
+            return prevState ? {
+                ...prevState,
+                name: action.payload.newName
+            } : null
+        case newSoundActionType.ToggleHidden:
+            return prevState ? {
+                ...prevState,
+                hidden: !prevState.hidden
+            } : null
+        case newSoundActionType.SetFile:
+            return prevState ? {
+                ...prevState,
+                file: action.payload.file
+            } : null
+    }
+}
+
 
 const MaxFileSize = 204800
 
@@ -33,15 +99,17 @@ const SoundsPage: NextPage = () => {
     const { guildid } = router.query
     const [dragging, setDragging] = useState(false)
     const [dragError, setDragError] = useState(null as DragError | null)
-    const [newSound, setNewSound] = useState(null as NewSound | null)
+    const [newSound, setNewSound] = useReducer(newSoundReducer, null as NewSound | null)
 
     const sounds = trpc.discord.sounds.useQuery({ guildid: typeof guildid === "string" ? guildid : "" }, { enabled: typeof guildid === "string" })
     return (
-        <div className="h-full w-full"
+        <div className="h-full w-full bg-slate-400 z-10"
             onDragOver={event => {
+                console.log("DragOver")
                 event.preventDefault()
             }}
             onDragEnter={ev => {
+                console.log("DragEnter")
                 ev.preventDefault()
                 setDragError(null)
                 const items = ev.dataTransfer.items
@@ -62,6 +130,7 @@ const SoundsPage: NextPage = () => {
                 setDragging(true)
             }}
             onDrop={ev => {
+                console.log("Drop")
                 ev.preventDefault()
                 setDragging(false)
                 if (dragError) {
@@ -77,13 +146,15 @@ const SoundsPage: NextPage = () => {
                     setDragError(DragError.Filesize)
                     return
                 }
-                setNewSound(createNewSound(file))
+                setNewSound({ type: newSoundActionType.InitFromFile, payload: { file } })
             }}
             onDragEnd={() => {
+                console.log("DragEnd")
                 setDragError(null)
                 setDragging(false)
             }}
             onDragLeave={() => {
+                console.log("DragLeave")
                 setDragError(null)
                 setDragging(false)
             }}
@@ -93,40 +164,78 @@ const SoundsPage: NextPage = () => {
                     Sounds
                 </title>
             </Head>
-            <p>Sounds Page</p>
-            {sounds.data &&
-                <table className="table-auto border border-collapse m-1 divide-x">
-                    <thead>
-                        <tr>
-                            <th className="border p-1">Name</th>
-                            <th className="border p-1">Hidden</th>
-                            <th className="border p-1">Actions</th>
-                        </tr>
-                    </thead>
-                    <tbody className="divide-y">
-                        {sounds.data.sounds.map(sound => (
-                            <tr key={sound.soundid} className="divide-x">
-                                <td>{sound.name}</td>
-                                <td>
-                                    {!sound.hidden ? <EyeSlashIcon className="max-h-6" /> : <EyeIcon className="max-h-6" />}
-
-                                </td>
-                                <td>
-                                    <div>
-                                        <span className="cursor-pointer ring-2 rounded m-2 select-none">Delete</span>
-                                        <span>Toggle visibility</span>
-                                    </div>
-                                </td>
+            <div>
+                <p>Sounds Page</p>
+                {sounds.data && !(dragging || dragError || newSound) &&
+                    <table className="table-auto border border-collapse m-1 divide-x">
+                        <thead>
+                            <tr>
+                                <th className="border p-1">Name</th>
+                                <th className="border p-1">Hidden</th>
+                                <th className="border p-1">Actions</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
-            }
-            {sounds.isLoading &&
-                <p>Loading ...</p>
-            }
-            {dragging && <p>DRAGGING!!!</p>}
-            {dragError && <p>Drag Error {dragError}</p>}
+                        </thead>
+                        <tbody className="divide-y">
+                            {sounds.data.sounds.map(sound => (
+                                <tr key={sound.soundid} className="divide-x">
+                                    <td>{sound.name}</td>
+                                    <td>
+                                        {!sound.hidden ? <EyeSlashIcon className="max-h-6" /> : <EyeIcon className="max-h-6" />}
+
+                                    </td>
+                                    <td>
+                                        <div>
+                                            <span className="cursor-pointer ring-2 rounded m-2 select-none">Delete</span>
+                                            <span>Toggle visibility</span>
+                                        </div>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                }
+                {sounds.isLoading &&
+                    <p>Loading ...</p>
+                }
+                {newSound &&
+                    <div className="flex flex-col gap-4">
+                        <p>NewSound</p>
+                        <div className="flex flex-col gap-4">
+                            <div>
+                                <label>Name</label>
+                                <input type="text" value={newSound.name} onChange={event => setNewSound({ type: newSoundActionType.SetName, payload: { newName: event.target.value } })} />
+                            </div>
+                            <div>
+                                <label>Hidden</label>
+                                <input type="checkbox" value={"" + newSound.hidden} onChange={() => setNewSound({ type: newSoundActionType.ToggleHidden })} />
+                            </div>
+                            <div>
+                                <label>File</label>
+                                {newSound.file ?
+                                    <div className="flex flex-row gap-4">
+                                        <p>{newSound.file.name}</p>
+                                        <button onClick={() => setNewSound({ type: newSoundActionType.SetFile, payload: { file: null } })} className="p-2 bg-cyan-400" >Clear file</button>
+                                    </div>
+                                    :
+                                    <input type="file" onChange={event => setNewSound({ type: newSoundActionType.SetFile, payload: { file: event.target.files?.[0] ?? null } })} />
+                                }
+                            </div>
+                            <div>
+                                <button>Create sound</button>
+                            </div>
+
+                        </div>
+                    </div>
+                }
+
+                {dragging &&
+                    <div className="h-full w-full flex-r">
+                        <p>Drop your file here to create a new sound</p>
+                    </div>
+                }
+                {dragError && <p>Drag Error: {dragError}</p>}
+            </div>
+
         </div>
 
     )
